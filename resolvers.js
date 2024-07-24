d03ee08531752fc9c664d75d8ca32130e1a4d095
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-07-24 22:39:56 +0530

    Add files via upload

--- resolvers.js
+++ resolvers.js
@@ -4,66 +4,61 @@ import { createJob, deleteJob, getJob, getJobs, getJobsByCompany, updateJob } fr
 
 export const resolvers = {
     Query: {
-        company: async (_root, { id }) => {
-            const company = await getCompany(id);
-            if (!company) {
-                throw notFoundError('No company found with id ' + id,);
-            }
-            return company;
-        },
-        job: async (_root, { id }) => {
-            const job = await getJob(id);
-            if(!job){
-                throw notFoundError ('No job found by id' + id,)
-            }
-            return job;
-        },
-        
-        jobs: () => getJobs(),
+      company: async (_root, { id }) => {
+        const company = await getCompany(id);
+        if (!company) {
+          throw notFoundError('No company found with id ' + id);
+        }
+        return company;
+      },
+      job: async (_root, { id }) => {
+        const job = await getJob(id);
+        if (!job) {
+          throw notFoundError('No job found with id ' + id);
+        }
+        return job;
+      },
+      jobs: () => getJobs(),
     },
- 
     Mutation: {
-        createJob: (_root, {input: {title, description} }, { user }) => {
-            if(!user) {
-                throw unauthorizedError ("Missing authoritation");
-            }
-             return createJob({ companyId: user.companyId, title, description })
-        },
-        
-        deleteJob: async (_root, { id }, {user}) => {
-            if(!user){
-                throw unauthorizedError('Missing authentication');
-            }
-            const job = await deleteJob(id);
-            if(!job) {
-                throw notFoundError('No Job found with id '+ id);
-            }
-            return job;
-        },
-
-        updateJob: async (_root, {input: { id, title, description} }, { user }) => {
-            if(!user) {
-                throw unauthorizedError('Missing Authentication');
-            }
-            const job = await updateJob({ id, companyId: user.companyId, title, description });
-            if(!job) {
-                throw notFoundError('No Job found with id' + id);
-            }
-            return job;
-        },
+      createJob: async (_root, { input: { title, description } }, { user }) => {
+        if (!user) {
+          throw unauthorizedError('Missing authentication');
+        }
+        return createJob({ companyId: user.companyId, title, description });
+      },
+      deleteJob: async (_root, { id }, { user }) => {
+        if (!user) {
+          throw unauthorizedError('Missing authentication');
+        }
+        const job = await deleteJob(id);
+        if (!job) {
+          throw notFoundError('No job found with id ' + id);
+        }
+        return job;
+      },
+      updateJob: async (_root, { input: { id, title, description } }, { user }) => {
+        if (!user) {
+          throw unauthorizedError('Missing authentication');
+        }
+        const job = await updateJob({ id, companyId: user.companyId, title, description });
+        if (!job) {
+          throw notFoundError('No job found with id ' + id);
+        }
+        return job;
+      },
     },
-
-
-
     Company: {
-        jobs: (company) => getJobsByCompany(company.id),
+      jobs: (company) => getJobsByCompany(company.id),
     },
-
     Job: {
-        company: (job) => getCompany(job.companyId),
-        date: (job) => toIsoDate(job.createdAt),
+      company: (job, _args, { companyLoader }) => {
+        return companyLoader.load(job.companyId);
     },
-};
+      date: (job) => toIsoDate(job.createdAt),
+    },
+  };
+
 
 function notFoundError (message) {
     return new GraphQLError(message, {
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import {readFile } from 'node:fs/promises';
 import { authMiddleware, handleLogin } from './auth.js';
 import { resolvers } from './resolvers.js';
 import { getUser } from './db/users.js';
+import { createCompanyLoader } from './db/companies.js';
 
 const PORT = 9000;
 
@@ -17,11 +18,12 @@ app.post('/login', handleLogin);
 const typeDefs =  await readFile('./schema.graphql', 'utf8');
 
 async function getContext({ req }) {
+  const companyLoader = createCompanyLoader();
+  const context = { companyLoader};
   if (req.auth){
     const user = await getUser(req.auth.sub);
-    return {user};
   }
-  return {};
+  return context;
 }
 
 const apolloServer = new ApolloServer ({ typeDefs, resolvers });

aef037da37b5b045194ef76ac581a11df20689c6
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-07-28 21:54:24 +0530

    WS - Client added

---
+++ client.js
@@ -0,0 +1,35 @@
+import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient as createWsClient } from 'graphql-ws';
+import { getAccessToken } from '../auth';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
+
+const authLink = new ApolloLink((operation, forward) => {
+  const accessToken = getAccessToken();
+  if (accessToken) {
+    operation.setContext({
+      headers: { 'Authorization': `Bearer ${accessToken}` },
+    });
+  }
+  return forward(operation);
+});
+
+const httpLink = concat(authLink, createHttpLink({ 
+  uri: 'http://localhost:9000/graphql' 
+}));
+
+const wsLink = new GraphQLWsLink(createWsClient({
+  url: 'ws://localhost:9000/graphql',
+}));
+
+function isSubscription(operation) {
+  const definition = getMainDefinition(operation.query);
+  return definition.kind === Kind.OPERATION_DEFINITION
+    && definition.operation === OperationTypeNode.SUBSCRIPTION;
+}
+
+export const apolloClient = new ApolloClient({
+  link: split(isSubscription, wsLink, httpLink),
+  cache: new InMemoryCache(),
+});

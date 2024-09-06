477e7d8402eaf2e56d8be25429e7ded54cb70979
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-09-06 22:06:31 +0530

    2024-09-06

--- resolvers.js
+++ resolvers.js
@@ -1,35 +1,77 @@
-import { GraphQLError } from 'graphql';
-import { PubSub } from 'graphql-subscriptions';
-import { createMessage, getMessages } from './db/messages.js';
-
-const pubSub = new PubSub();
+import { GraphQLError } from "graphql";
+import { getCompany } from "./db/companies.js";
+import { createJob, deleteJob, getJob, getJobs, getJobsByCompany, updateJob } from "./db/jobs.js";
 
 export const resolvers = {
-  Query: {
-    messages: (_root, _args, { user }) => {
-      if (!user) throw unauthorizedError();
-      return getMessages();
+    Query: {
+      company: async (_root, { id }) => {
+        const company = await getCompany(id);
+        if (!company) {
+          throw notFoundError('No company found with id ' + id);
+        }
+        return company;
+      },
+      job: async (_root, { id }) => {
+        const job = await getJob(id);
+        if (!job) {
+          throw notFoundError('No job found with id ' + id);
+        }
+        return job;
+      },
+      jobs: (_root, { limit, offset }) => getJobs(limit, offset),
     },
-  },
-
-  Mutation: {
-    addMessage: async (_root, { text }, { user }) => {
-      if (!user) throw unauthorizedError();
-      const message = await createMessage(user, text);
-      pubSub.publish('MESSAGE_ADDED', { messageAdded: message });
-      return message;
+    Mutation: {
+      createJob: async (_root, { input: { title, description } }, { user }) => {
+        if (!user) {
+          throw unauthorizedError('Missing authentication');
+        }
+        return createJob({ companyId: user.companyId, title, description });
+      },
+      deleteJob: async (_root, { id }, { user }) => {
+        if (!user) {
+          throw unauthorizedError('Missing authentication');
+        }
+        const job = await deleteJob(id);
+        if (!job) {
+          throw notFoundError('No job found with id ' + id);
+        }
+        return job;
+      },
+      updateJob: async (_root, { input: { id, title, description } }, { user }) => {
+        if (!user) {
+          throw unauthorizedError('Missing authentication');
+        }
+        const job = await updateJob({ id, companyId: user.companyId, title, description });
+        if (!job) {
+          throw notFoundError('No job found with id ' + id);
+        }
+        return job;
+      },
     },
-  },
-
-  Subscription: {
-    messageAdded: {
-      subscribe: () => pubSub.asyncIterator('MESSAGE_ADDED'),
+    Company: {
+      jobs: (company) => getJobsByCompany(company.id),
     },
-  },
-};
+    Job: {
+      company: (job, _args, { companyLoader }) => {
+        return companyLoader.load(job.companyId);
+    },
+      date: (job) => toIsoDate(job.createdAt),
+    },
+  };
 
-function unauthorizedError() {
-  return new GraphQLError('Not authenticated', {
-    extensions: { code: 'UNAUTHORIZED' },
-  });
+
+function notFoundError (message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'NOT_FOUND' },
+    });
+}
+
+function unauthorizedError (message) {
+    return new GraphQLError(message, {
+        extensions: { code: 'Unauthorized' },
+    });
+}
+
+function toIsoDate(value) {
+    return value.slice(0, 'yyyy-mm-dd'.length);
 }
